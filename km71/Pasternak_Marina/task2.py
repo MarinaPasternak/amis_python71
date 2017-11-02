b9a46d9d1f322c06eb36812689db7df7f7bcdb58
Marina_Pasternak <31963187+MarinaPasternak@users.noreply.github.com>
2017-11-02 21:30:36 +0200

    Homework_2

---
+++ km71/Pasternak_Marina/task2.py
@@ -0,0 +1,6 @@
+x=float(input("Enter x: "))
+if x>0: answer = 1
+if x<0: answer = -1
+if x=0: answer = 0
+print("sign(x)= ",answer)
+input()
---
+++ km71/Pasternak_Marina/task6.py
@@ -0,0 +1,9 @@
+i1=int(input("Line number of the first cell"))
+j1=int(input("Column number of the first сell"))
+i2=int(input("Line number of the second cell"))
+j2=int(input("Column number of the second cell"))
+if i1==i2 or j1==j2:
+	print("YES")
+else:
+	print("NO")
+input()
